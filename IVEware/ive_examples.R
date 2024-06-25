ba4347eecf95844c40d1e0b8e4f30c14af2fb56d
Brad Burkman <bburkman@lsmsa.edu>
2024-06-25 12:10:06 -0500

    Thirteenth Commit

--- IVEware/ive_examples.R
+++ IVEware/ive_examples.R
@@ -23,5 +23,5 @@ save(CRSS, file = "CRSS.rda")
 impute(name="impute")
 
 data <- load(file = "impute.rda")
-write.csv(get(data), file="../../Big_Files/data_IVEware_seed_0.csv")
+write.csv(get(data), file="../../Big_Files/data_IVEware.csv")
 
